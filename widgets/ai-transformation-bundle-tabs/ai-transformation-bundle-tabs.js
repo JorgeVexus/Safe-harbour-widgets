67c8f9949d71f4451ece0700519871e9ee65a9d9
JorgeVexus <asahizv1@gmail.com>
2026-07-27 09:51:17 -0600

    fix: remove lorem ipsum header description from ai-transformation-bundle-tabs

--- widgets/ai-transformation-bundle-tabs/ai-transformation-bundle-tabs.js
+++ widgets/ai-transformation-bundle-tabs/ai-transformation-bundle-tabs.js
@@ -632,7 +632,6 @@
       <div class="atb-inner">
         <header class="atb-header">
           <h1 class="atb-heading">AI Transformation Bundle</h1>
-          <p class="atb-desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
         </header>
 
         <div class="atb-layout">
